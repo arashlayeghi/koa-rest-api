850dbf8a15bb5c3c7e198eb57f0f35da2f4ec5c7
Arash Layeghi <arashlayeghi57@gmail.com>
2018-07-16 16:39:01 +0430

    add even handlers and setting status codes

--- app.js
+++ app.js
@@ -13,61 +13,100 @@ const securedRouter = new Router();
 
 app.use(BodyParser());
 app.use(logger());
-// app.use(JWT.errorHandler()).use(JWT.jwt());
 
 // Apply JWT middleware to secured router only
 securedRouter.use(JWT.errorHandler()).use(JWT.jwt());
 
 router.post("/auth", async (ctx) => {
-    let username = ctx.request.body.username;
-    let password = ctx.request.body.password;
+  let username = ctx.request.body.username;
+  let password = ctx.request.body.password;
 
-    if (username === "user" && password === "pwd") {
-        ctx.body = {
-            token: JWT.issue({
-                user: "user",
-                role: "admin"
-            })
-        };
-    } else {
-        ctx.status = 401;
-        ctx.body = {error: "Invalid login"}
+  // This is just for demonstration! Username and password
+  // must come from database and password musu be hashed
+  if (username === "user" && password === "pwd") {
+    ctx.body = {
+      token: JWT.issue({
+        user: "user",
+        role: "admin"
+      })
+    };
+  } else {
+    ctx.status = 401;
+    ctx.body = {
+      error: "Invalid login"
     }
+  }
 });
-
-// router.post('/', async ctx => {
-//   let name = ctx.request.body.name || "World";
-//   ctx.body = {
-//     message: `Hello ${name}!`
-//   };
-// });
 
 // List all people
 securedRouter.get("/people", async (ctx) => {
-    ctx.body = await ctx.app.people.find().toArray();
+  let people;
+  try {
+    people = await ctx.app.people.find().toArray();
+  } catch (e) {
+    ctx.body = "An error occured during getting all people: " + e;
+    ctx.status = 401;
+  }
+  ctx.body = people;
+  ctx.status = 200; // OK
 });
 
 // Get one
 securedRouter.get("/people/:id", async (ctx) => {
-    ctx.body = await ctx.app.people.findOne({"_id": ObjectID(ctx.params.id)});
+  let person;
+  try {
+    person = await ctx.app.people.findOne({"_id": ObjectID(ctx.params.id)});
+  } catch (e) {
+      ctx.body = "An error occured during getting person with id: " + ctx.params.id + " with the error of: " + e;
+      ctx.status = 401;
+  }
+
+  ctx.body = person;
+  ctx.status = 200; // OK
+
+
 });
 
 // Create new person
 securedRouter.post("/people", async (ctx) => {
-    ctx.body = await ctx.app.people.insert(ctx.request.body);
+  let person;
+  try {
+    person = await ctx.app.people.insert(ctx.request.body);
+  } catch (e) {
+    ctx.body = "An error occured during creating a new person: " + e;
+    ctx.status = 401;
+  }
+  ctx.body = person;
+  ctx.status = 201; // CREATED OK
 });
 
 // Update one
 securedRouter.put("/people/:id", async (ctx) => {
-    let documentQuery = {"_id": ObjectID(ctx.params.id)}; // Used to find the document
-    let valuesToUpdate = ctx.request.body;
-    ctx.body = await ctx.app.people.updateOne(documentQuery, {$set: valuesToUpdate});
+  let updatedPerson;
+  let documentQuery = {"_id": ObjectID(ctx.params.id)}; // Used to find the document
+  let valuesToUpdate = ctx.request.body;
+  try {
+    updatedPerson = await ctx.app.people.updateOne(documentQuery, {$set: valuesToUpdate});
+  } catch (e) {
+    ctx.body = "An error occured during updating a person with id: " + ctx.params.id +  " with error of:" + e;
+    ctx.status = 401;
+  }
+  ctx.body = updatedPerson;
+  ctx.status = 200; // OK
 });
 
 // Delete one
 securedRouter.delete("/people/:id", async (ctx) => {
-    let documentQuery = {"_id": ObjectID(ctx.params.id)}; // Used to find the document
-    ctx.body = await ctx.app.people.deleteOne(documentQuery);
+  let deletedPerson;
+  let documentQuery = {"_id": ObjectID(ctx.params.id)}; // Used to find the document
+  try {
+    deletedPerson = await ctx.app.people.deleteOne(documentQuery);
+  } catch (e) {
+    ctx.body = "An error occured during the deletion of a person with the id of: " + ctx.params.id + "with the error of:" + e;
+    ctx.status = 401;
+  }
+  ctx.body = deletedPerson;
+  ctx.status = 200;
 });
 
 app.use(router.routes()).use(router.allowedMethods());
