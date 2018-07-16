1f7ac9414cf974f7dc6f4aee1c2152853b8941e0
Arash Layeghi <arashlayeghi57@gmail.com>
2018-07-16 16:39:50 +0430

    Update jwt.js

--- jwt.js
+++ jwt.js
@@ -4,20 +4,20 @@ const SECRET = "S3cRET~!";
 const jwtInstance = jwt({secret: SECRET});
 
 function JWTErrorHandler(ctx, next) {
-    return next().catch((err) => {
-        if (401 == err.status) {
-            ctx.status = 401;
-            ctx.body = {
-                "error": "Not authorized"
-            };
-        } else {
-            throw err;
-        }
-    });
+  return next().catch((err) => {
+    if (401 == err.status) {
+      ctx.status = 401;
+      ctx.body = {
+        "error": "Not authorized"
+      };
+    } else {
+      throw err;
+    }
+  });
 };
 
 module.exports.jwt = () => jwtInstance;
 module.exports.errorHandler = () => JWTErrorHandler;
-module.exports.issue =  (payload) => {
-    return jsonwebtoken.sign(payload, SECRET);
+module.exports.issue = (payload) => {
+  return jsonwebtoken.sign(payload, SECRET);
 };
